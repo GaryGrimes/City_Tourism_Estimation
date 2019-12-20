620e1a604c193b0940aa4e256a58127450318a15
Gary <shenkai.gary.grimes@gmail.com>
2019-12-21 00:29:55 +0900

    Modified readme

--- Happy_Github.py
+++ Happy_Github.py
@@ -1,3 +1,5 @@
-''' This script was just created for fun. To record happiness of learning and creating something new.'''
+''' This script was just created for fun. To record happiness of learning and creating something new.
+This is version 2.0 of city tourism estimation project'''
+
 
 print("Let's go learning something")
