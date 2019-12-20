f3298c5cecbc4f4cfa15341c8b9cc5e3377e4ec5
Gary <shenkai.gary.grimes@gmail.com>
2019-12-21 00:42:54 +0900

    Revert "Create README.md"
    This reverts commit 1681fa92

--- Happy_Github.py
+++ Happy_Github.py
@@ -1,5 +1,4 @@
-''' This script was just created for fun. To record happiness of learning and creating something new.
-This is version 2.0 of city tourism estimation project'''
+''' This script was just created for fun. To record happiness of learning and creating something new'''
 
 
 print("Let's go learning something")
